74e1c394d3e286d7ae06118d8c536e4ad1de2445
Alex Zverev <lordzver007@yandex.ru>
2022-03-29 00:12:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2.9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp2._9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(1, 27);
+            int amountOfNumbers = 0;
+
+            Console.WriteLine(number);
+            
+            for (int i = 100; i < 999; i++)
+            {
+                for(int j = i; j >= 0; j-= number)
+                {
+                    if (j == 0)
+                    {
+                        amountOfNumbers++;
+                    }
+                }
+                               
+            }
+            Console.WriteLine(amountOfNumbers);
+        }
+    }
+}
